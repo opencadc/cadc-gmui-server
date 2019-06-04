35a6b7491e9d64b80732e9eb338f2f9f3ce4022d
Nishchint Dhawan <nishchint34@gmail.com>
2019-06-04 09:42:31 -0700

    member delete reset formand success

--- src/main/resources/META-INF/resources/js/cadc.gms.js
+++ src/main/resources/META-INF/resources/js/cadc.gms.js
@@ -417,9 +417,12 @@
         method: 'DELETE'
       })
         .done(function(message) {
+          colorSuccess()
           adminDeleted(_adminID)
+          resetForm("admin")
         })
         .fail(function(xhr, options, error) {
+          colorFail()
           trigger(cadc.web.gms.events.onAdminDeletedError, {
             textStatus: options,
             error: error,
@@ -601,11 +604,15 @@
       $.ajax({
         url: deleteURL,
         method: 'DELETE'
+        	
       })
         .done(function(message) {
+          colorSuccess()
           memberDeleted(_memberID)
+          resetForm("member")
         })
         .fail(function(xhr, options, error) {
+          colorFail()
           trigger(cadc.web.gms.events.onMemberDeletedError, {
             textStatus: options,
             error: error,
